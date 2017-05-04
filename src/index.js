d27d36dce4e17e95c146373d8450cf5285c16b5d
Jeong Min Lee <jacklee0223@gmail.com>
2017-05-04 13:31:40 -0700

    add searchbar

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
+import SearchBar from './components/search_bar'
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 const API_KEY = 'AIzaSyBWDm6PrY1szVMjkcxv2ruYC_flXh3inPA';
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
+  <div class="container">
+	  <Provider store={createStoreWithMiddleware(reducers)}>
+	    <App />
+	  </Provider>
+    <SearchBar />
+  </div>
   , document.querySelector('.container'));
