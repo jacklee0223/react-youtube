b33c1337d5b539ccf1ae576f0b563398fcf7a7cd
Jeong Min Lee <jacklee0223@gmail.com>
2017-05-04 13:34:55 -0700

    move searchbar tag into app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
+import SearchBar from './search_bar'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      	React simple starter
+      	<h3>
+	      	React simple starter
+	    </h3>
+	    <SearchBar />
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
-import SearchBar from './components/search_bar'
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 const API_KEY = 'AIzaSyBWDm6PrY1szVMjkcxv2ruYC_flXh3inPA';
 
 ReactDOM.render(
-  <div class="container">
+  <div className="container">
 	  <Provider store={createStoreWithMiddleware(reducers)}>
 	    <App />
 	  </Provider>
-    <SearchBar />
   </div>
   , document.querySelector('.container'));
